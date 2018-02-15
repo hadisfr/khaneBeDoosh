81657868b478b36c678fa08d475879fc411262af
Mohammad Reza <mthcom@live.com>
2018-02-15 11:35:00 +0330

    Add search servlet

---
+++ src/main/java/SearchServlet.java
@@ -0,0 +1,23 @@
+package main.java;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class SearchServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        ArrayList<House> houses = new ArrayList<House>();
+        House house = new House("398y2iuwjndwksfsd", 200, BuildingType.APARTMENT, "http://google.com",
+                DealType.BUY, 10, 20, 30, null);
+        houses.add(house);
+        request.setAttribute("houses", houses);
+        request.getRequestDispatcher("searchResults.jsp").forward(request, response);
+    }
+}
