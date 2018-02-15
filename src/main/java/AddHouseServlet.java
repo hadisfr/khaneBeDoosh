c76febe95ffffa35ff08d401e1e767368e3fc708
h@di <hadi@hadisafari.ir>
2018-02-15 15:28:25 +0330

    debug AddHouseServlet

--- src/main/java/AddHouseServlet.java
+++ src/main/java/AddHouseServlet.java
@@ -8,33 +8,33 @@ import java.io.IOException;
 
 public class AddHouseServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // DealType dealType = DealType.parseString(request.getParameter("dealType"));
-        // try {
-        //     if(DealType == DealType.RENT) {
-        //         KhaneBeDoosh.getInstance().addHouse(
-        //             "String id,",
-        //             Integer.parseInt(request.getParameter("area") == "" ? "0" : request.getParameter("area")),
-        //             BuildingType.parseString(request.getParameter("buildingType")),
-        //             "",
-        //             null,
-        //             Integer.parseInt(request.getParameter("price") == "" ? "0" : request.getParameter("price")),
-        //             0
-        //         );
-        //     } else if(buildingType == BuildingType.SELL) {
-        //         KhaneBeDoosh.getInstance().addHouse(
-        //             "String id,",
-        //             Integer.parseInt(request.getParameter("area") == "" ? "0" : request.getParameter("area")),
-        //             BuildingType.parseString(request.getParameter("buildingType")),
-        //             "",
-        //             null,
-        //             Integer.parseInt(request.getParameter("price") == "" ? "0" : request.getParameter("price"))
-        //         );
-        //     } else
-        //         throw new IOException("Bad BuildingType");
-        // } catch(Exception e) {
-        //     request.setAttribute("msg", "استثنا: ‪" + e);
-        //     request.getRequestDispatcher("/").forward(request, response);
-        // }
+        DealType dealType = DealType.parseString(request.getParameter("dealType"));
+        try {
+            if(dealType == DealType.RENT) {
+                KhaneBeDoosh.getInstance().addHouse(
+                    "String id,",
+                    Integer.parseInt(request.getParameter("area") == "" ? "0" : request.getParameter("area")),
+                    BuildingType.parseString(request.getParameter("buildingType")),
+                    "",
+                    null,
+                    Integer.parseInt(request.getParameter("price") == "" ? "0" : request.getParameter("price")),
+                    0
+                );
+            } else if(dealType == DealType.SELL) {
+                KhaneBeDoosh.getInstance().addHouse(
+                    "String id,",
+                    Integer.parseInt(request.getParameter("area") == "" ? "0" : request.getParameter("area")),
+                    BuildingType.parseString(request.getParameter("buildingType")),
+                    "",
+                    null,
+                    Integer.parseInt(request.getParameter("price") == "" ? "0" : request.getParameter("price"))
+                );
+            } else
+                throw new IOException("Bad BuildingType");
+        } catch(Exception e) {
+            request.setAttribute("msg", "استثنا: ‪" + e);
+            request.getRequestDispatcher("/").forward(request, response);
+        }
         request.getRequestDispatcher("/").forward(request, response);
     }
 
