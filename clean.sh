8bcda5163fcb3f452755ced615cac93302067f0f
h@di <hadi@hadisafari.ir>
2018-03-02 12:12:24 +0330

    debug scripts

--- clean.sh
+++ clean.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+rm -rf target
 if [[ CATALINA_HOME ]]; then
     rm -rf $CATALINA_HOME/webapps/khaneBeDoosh*
 fi
--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
+rm -rf target
 mvn tomcat7:redeploy && echo -e "see \033[4mhttp://localhost:8080/khaneBeDoosh\033[0m"
