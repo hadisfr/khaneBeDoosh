247c21eaec800fe86ba37a0676f3afa9ec1b998f
h@di <hadi@hadisafari.ir>
2018-03-18 02:50:46 +0330

    change csses due to prevent conflict

--- front-end/src/Pay/Pay.js
+++ front-end/src/Pay/Pay.js
@@ -6,7 +6,7 @@ import PayForm from './PayForm'
 class Pay extends Component {
     render() {
         return (
-            <div className="row">
+            <div className="row pay">
                 <div className="col-12 col-lg-6 current-credit">
                     <span className="gray">اعتبار کنونی </span> {this.props.user.balance} <span className="gray"> تومان</span>
                 </div>
