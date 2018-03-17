6ca714d00ce7d98acdd93756b6e45f3e1bebaf9e
h@di <hadi@hadisafari.ir>
2018-03-17 13:34:21 +0330

    change directory structure

---
+++ front-end/src/App/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { Route, withRouter } from 'react-router-dom'
+import './App.css';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <h1 className="App-title">KhaneBeDoosh</h1>
+                </header>
+                <p className="App-intro">
+                    Under Construction...
+                </p>
+            </div>
+        );
+    }
+}
+
+export default App;
