84a8d929090c10df87371bea0bbf9a19fa333211
h@di <hadi@hadisafari.ir>
2018-03-17 18:31:31 +0330

    make PayForm react-compatible

--- front-end/src/Pay/PayForm.js
+++ front-end/src/Pay/PayForm.js
@@ -3,12 +3,41 @@ import { withRouter } from 'react-router-dom'
 import './Pay.css'
 
 class PayForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            balance: "",
+        };
+    }
+
+    handleChange(event) {
+        event.preventDefault();
+        this.setState({balance: event.target.value});
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        alert(this.state.balance);
+    }
+
     render() {
         return (
-            <form>
-                <input type="number" name="balance" pattern="[0-9]+" placeholder="مبلغ مورد نظر" />
+            <form onSubmit={(event) => this.handleSubmit(event)}>
+                <input
+                    type="number"
+                    name="balance"
+                    pattern="[0-9]+"
+                    placeholder="مبلغ مورد نظر"
+                    onChange={(event) => this.handleChange(event)}
+                    value={this.state.balance}
+                />
                 <span className="badge">تومان</span>
-                <input type="submit" value="افزایش اعتبار" className="btn btn-green" />
+                <input
+                    type="submit"
+                    value="افزایش اعتبار"
+                    className="btn btn-green"
+                    disabled={!(this.state.balance && this.state.balance > 0)}
+                />
             </form>
         );
     }
