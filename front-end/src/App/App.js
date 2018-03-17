411002c0e0b57bfe75416443e6265da7948edc42
h@di <hadi@hadisafari.ir>
2018-03-17 14:58:43 +0330

    move some csses to react

--- front-end/src/App/App.js
+++ front-end/src/App/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import { Route, withRouter } from 'react-router-dom'
 import Footer from '../Footer/Footer'
+import './main.css';
+import './boxes.css';
+import './boxes.css';
+import './btn.css';
 
 class App extends Component {
     render() {
