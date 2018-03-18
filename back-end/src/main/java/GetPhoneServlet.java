d6711d3d6d8923d2bc560e88b21ee2041874acc4
h@di <hadi@hadisafari.ir>
2018-03-19 00:52:15 +0330

    rewrite GetPhoneServlet

--- back-end/src/main/java/GetPhoneServlet.java
+++ back-end/src/main/java/GetPhoneServlet.java
@@ -16,28 +16,26 @@ public class GetPhoneServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        HashMap<String, String> res = new HashMap<String, String>();
         try {
-//            response.setContentType("application/json");
-//            response.setCharacterEncoding("UTF-8");
-//            HashMap<String, Boolean> res = new HashMap<String, Boolean>();
-            House house = KhaneBeDoosh.getInstance().getHouseById(
-                    request.getParameter("houseId"),
-                    Integer.parseInt(request.getParameter("ownerId"))
-            );
-            request.setAttribute("house", house);
             Individual currentUser = ((Individual) KhaneBeDoosh.getInstance().getDefaultUser());
             String houseId = request.getParameter("houseId");
             int ownerId = Integer.parseInt(request.getParameter("ownerId"));
-            request.setAttribute("wantsToSeePhone", true);
-            request.setAttribute("canSeePhone",
-                    currentUser.hasPaidforHouse(houseId, ownerId) || currentUser.payForHouse(houseId, ownerId));
-            request.getRequestDispatcher("houseDetails.jsp").forward(request, response);
-//            response.getWriter().write((new Gson()).toJson(res));
+            House house = KhaneBeDoosh.getInstance().getHouseById(houseId, ownerId);
+            response.setStatus(
+                    currentUser.hasPaidforHouse(houseId, ownerId) || currentUser.payForHouse(houseId, ownerId)
+                            ? HttpServletResponse.SC_OK
+                            : HttpServletResponse.SC_PAYMENT_REQUIRED
+            );
+            if (response.getStatus() == HttpServletResponse.SC_OK)
+                res.put("phone", house.getPhone());
+            response.getWriter().write((new Gson()).toJson(res));
         } catch (Exception e) {
             response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-            HashMap<String, String> err_res = new HashMap<String, String>();
-            err_res.put("msg", e.toString());
-            response.getWriter().write((new Gson()).toJson(err_res));
+            res.put("msg", e.toString());
+            response.getWriter().write((new Gson()).toJson(res));
         }
     }
 }
