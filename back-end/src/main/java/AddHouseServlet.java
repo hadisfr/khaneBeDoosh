575393bb6687eb1600eec96a002b05b73abdcdc4
h@di <hadi@hadisafari.ir>
2018-03-18 22:24:03 +0330

    rewrite AddHouseServlet

--- back-end/src/main/java/AddHouseServlet.java
+++ back-end/src/main/java/AddHouseServlet.java
@@ -17,45 +17,69 @@ public class AddHouseServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        // TODO: should handle default params? if so, handle them properly.
         response.setContentType("application/json");
         response.setCharacterEncoding("UTF-8");
-        DealType dealType = DealType.parseString(request.getParameter("dealType"));
         try {
             String id = RandomStringUtils.randomAlphabetic(20);
             String imageUrl = "";
             String expireTime = "2032-12-01";
             User currentUser = KhaneBeDoosh.getInstance().getDefaultUser();
+            DealType dealType = request.getParameterMap().containsKey("dealType")
+                    ? DealType.parseString(request.getParameter("dealType"))
+                    : DealType.SELL;
+            int area = request.getParameterMap().containsKey("area")
+                    ? Integer.parseInt(request.getParameter("area"))
+                    : 0;
+            BuildingType buildingType = request.getParameterMap().containsKey("buildingType")
+                    ? BuildingType.parseString(request.getParameter("buildingType"))
+                    : BuildingType.APARTMENT;
+            if(buildingType == null)
+                throw new IOException("Bad BuildingType");
+            int price =  request.getParameterMap().containsKey("price")
+                    ? Integer.parseInt(request.getParameter("price"))
+                    : 0;
+            String address = request.getParameterMap().containsKey("address")
+                    ? request.getParameter("address")
+                    : "";
+            String phone = request.getParameterMap().containsKey("phone")
+                    ? request.getParameter("phone")
+                    : "";
+            String description = request.getParameterMap().containsKey("description")
+                    ? request.getParameter("description")
+                    : "";
             if (dealType == DealType.RENT) {
                 KhaneBeDoosh.getInstance().addHouse(
                         id,
-                        Integer.parseInt(request.getParameter("area") == "" ? "0" : request.getParameter("area")),
-                        BuildingType.parseString(request.getParameter("buildingType")),
+                        area,
+                        buildingType,
                         imageUrl,
                         currentUser,
-                        Integer.parseInt(request.getParameter("price") == "" ? "0" : request.getParameter("price")),
+                        price,
                         0,
-                        request.getParameter("address"),
-                        request.getParameter("phone"),
-                        request.getParameter("description"),
+                        address,
+                        phone,
+                        description,
                         expireTime
                 );
-                request.setAttribute("msg", "خانه با موفقیت اضافه شد.");
             } else if (dealType == DealType.SELL) {
                 KhaneBeDoosh.getInstance().addHouse(
                         id,
-                        Integer.parseInt(request.getParameter("area") == "" ? "0" : request.getParameter("area")),
-                        BuildingType.parseString(request.getParameter("buildingType")),
+                        area,
+                        buildingType,
                         imageUrl,
                         currentUser,
-                        Integer.parseInt(request.getParameter("price") == "" ? "0" : request.getParameter("price")),
-                        request.getParameter("address"),
-                        request.getParameter("phone"),
-                        request.getParameter("description"),
+                        price,
+                        address,
+                        phone,
+                        description,
                         expireTime
                 );
-                request.setAttribute("msg", "خانه با موفقیت اضافه شد.");
             } else
-                throw new IOException("Bad BuildingType");
+                throw new IOException("Bad DealType");
+            HashMap<String, Boolean> res = new HashMap<String, Boolean>();
+            res.put("success", true);
+            response.getWriter().write((new Gson()).toJson(res));
         } catch (Exception e) {
             response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
             HashMap<String, String> err_res = new HashMap<String, String>();
