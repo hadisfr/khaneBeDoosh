aa5cc2813c306f44371a5fd2cf12f9738daf93dd
Mohammad Reza <mthcom@live.com>
2018-03-19 17:52:40 +0330

    Change Decryption to a rudimentary function

--- back-end/src/main/java/Utility.java
+++ back-end/src/main/java/Utility.java
@@ -11,31 +11,34 @@ public class Utility {
     public static final int illegalSearchValue = -1;
 
     public static String encrypt(String houseId, int userId) {
-        String text = houseId + delimiter + userId;
-        Key aesKey = new SecretKeySpec(privateKey.getBytes(), "AES");
-        try {
-            Cipher cipher = Cipher.getInstance("AES");
-            cipher.init(Cipher.ENCRYPT_MODE, aesKey);
-            byte[] encrypted = cipher.doFinal(text.getBytes());
-            return new String(encrypted);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return null;
+//        String text = houseId + delimiter + userId;
+//        Key aesKey = new SecretKeySpec(privateKey.getBytes(), "AES");
+//        try {
+//            Cipher cipher = Cipher.getInstance("AES");
+//            cipher.init(Cipher.ENCRYPT_MODE, aesKey);
+//            byte[] encrypted = cipher.doFinal(text.getBytes());
+//            return new String(encrypted);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        return null;
+        return houseId + delimiter + userId;
     }
 
     public static IntStringPair decrypt(String text) {
-        Key aesKey = new SecretKeySpec(privateKey.getBytes(), "AES");
-        try {
-            Cipher cipher = Cipher.getInstance("AES");
-            cipher.init(Cipher.DECRYPT_MODE, aesKey);
-            String decrypted = new String(cipher.doFinal(text.getBytes()));
-            String[] decrypted_parts = decrypted.split(delimiter);
-            return new IntStringPair(Integer.parseInt(decrypted_parts[1]), decrypted_parts[0]);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return null;
+//        Key aesKey = new SecretKeySpec(privateKey.getBytes(), "AES");
+//        try {
+//            Cipher cipher = Cipher.getInstance("AES");
+//            cipher.init(Cipher.DECRYPT_MODE, aesKey);
+//            String decrypted = new String(cipher.doFinal(text.getBytes()));
+//            String[] decrypted_parts = decrypted.split(delimiter);
+//            return new IntStringPair(Integer.parseInt(decrypted_parts[1]), decrypted_parts[0]);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        return null;
+        String[] decrypted_parts = text.split(delimiter);
+        return new IntStringPair(Integer.parseInt(decrypted_parts[1]), decrypted_parts[0]);
     }
 
     public static ArrayList<House> filterHouses(ArrayList<House> houses, BuildingType buildingType, DealType dealType, int minArea, Price maxPrice) {
