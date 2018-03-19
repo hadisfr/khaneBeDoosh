2ee7ac61521d17b6efc50488f3a419f9cabd6bc8
Mohammad Reza <mthcom@live.com>
2018-03-19 17:36:52 +0330

    Add house detail wrapper

---
+++ back-end/src/main/java/HouseDetailWrapper.java
@@ -0,0 +1,27 @@
+package main.java;
+
+public class HouseDetailWrapper {
+    private String imageUrl;
+    private String id;
+    private int area;
+    private BuildingType buildingType;
+    private DealType dealType;
+    private Price price;
+    private String address;
+    private String description;
+    private String expireTime;
+
+    public HouseDetailWrapper(House house){
+        this.dealType = house.getDealType();
+        this.imageUrl = house.getImageUrl();
+        this.id = Utility.encrypt(house.getId(), house.getOwner().getId());
+        this.area = house.getArea();
+        this.buildingType = house.getBuildingType();
+        this.price = house.getPrice();
+        if(this.imageUrl.equals(""))
+            this.imageUrl = KhaneBeDoosh.getNopicUri();
+        this.address = house.getAddress();
+        this.description = house.getDescription();
+        this.expireTime = house.getExpireTime();
+    }
+}
