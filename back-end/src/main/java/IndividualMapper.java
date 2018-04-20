17aba59c15e7af9113844de648ef472dab81e9c3
Mohammad Reza <mthcom@live.com>
2018-04-20 16:54:19 +0430

    add individual mapper

---
+++ back-end/src/main/java/IndividualMapper.java
@@ -0,0 +1,12 @@
+package main.java;
+
+public class IndividualMapper {
+
+    public Individual getIndividualById(int id){
+        return null;
+    }
+
+    public Individual getIndividualByUsername(String username){
+        return null;
+    }
+}
