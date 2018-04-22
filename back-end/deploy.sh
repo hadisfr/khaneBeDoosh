43e27b554b9dddc3fe48da87c875adfa36807630
h@di <hadi@hadisafari.ir>
2018-04-22 18:11:24 +0430

    add createDB to deploy script

--- back-end/deploy.sh
+++ back-end/deploy.sh
@@ -5,5 +5,6 @@ mkdir src/main/webapp/doc
 apidoc -i src/main/java/ -o src/main/webapp/doc
 mvn tomcat7:redeploy && echo -e "see \033[4mhttp://localhost:8080/khaneBeDoosh\033[0m"
 pushd migrate > /dev/null
+./create.sh
 ./push.sh
 popd > /dev/null
