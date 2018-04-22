2f63f947751e9aa5f1fdfb303db567a466aec7b7
h@di <hadi@hadisafari.ir>
2018-04-22 18:25:25 +0430

    create Realestate table

--- back-end/migrate/migrate.sql
+++ back-end/migrate/migrate.sql
@@ -2,3 +2,4 @@ CREATE TABLE Individual ("username" TEXT PRIMARY KEY NOT NULL, "balance" INTEGER
 INSERT INTO "Individual" ("username","balance","displayName") VALUES ("behnam","200","بهنام همایون");
 CREATE TABLE PaidHouses ("individualId" TEXT NOT NULL, "ownerId" TEXT NOT NULL, "houseId" TEXT NOT NULL, PRIMARY KEY ("individualId", "ownerId", "houseId"));
 INSERT INTO "PaidHouses" ("individualId","ownerId","houseId") VALUES ("behnam","acm","78278842-78eb-475f-a38a-a7001f86f644");
+CREATE TABLE Realestate ("name" TEXT, "expireTime" DATETIME);
