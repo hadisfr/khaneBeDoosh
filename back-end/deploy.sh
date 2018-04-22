b9888acb1e8aecc3bc244ab8fb10fb1d6f3b7fef
h@di <hadi@hadisafari.ir>
2018-04-22 14:10:29 +0430

    add db push to deploy script

--- back-end/deploy.sh
+++ back-end/deploy.sh
@@ -4,3 +4,6 @@ rm -rf target src/main/webapp/doc
 mkdir src/main/webapp/doc
 apidoc -i src/main/java/ -o src/main/webapp/doc
 mvn tomcat7:redeploy && echo -e "see \033[4mhttp://localhost:8080/khaneBeDoosh\033[0m"
+pushd migrate > /dev/null
+./push.sh
+popd > /dev/null
