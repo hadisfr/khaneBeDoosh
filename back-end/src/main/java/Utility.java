4058de0bdac53be74017c5ad50e685083a51d685
Mohammad Reza <mthcom@live.com>
2018-05-10 16:47:01 +0430

    add hash functions

--- back-end/src/main/java/Utility.java
+++ back-end/src/main/java/Utility.java
@@ -2,12 +2,22 @@ package main.java;
 
 import io.jsonwebtoken.*;
 
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+import java.util.Arrays;
+import java.util.Base64;
+
 public class Utility {
     private Utility() {
     }
 
     private static final String secret = "Shahrbaraaz";
     public static final int IllegalSearchValue = -1;
+    private static final int ITERATIONS = 10000;
+    private static final int KEY_LENGTH = 256;
 
     public static String encryptHouseId(String houseId, String ownerId) {
         return Jwts.builder()
@@ -44,5 +54,36 @@ public class Utility {
         } catch (JwtException e) {
             throw new IllegalArgumentException("invalid token");
         }
+    }
+
+    public static String generateSalt() {
+        SecureRandom random = new SecureRandom();
+        byte[] salt = new byte[32];
+        random.nextBytes(salt);
+        return Base64.getEncoder().encodeToString(salt);
+    }
+
+    public static String hash(String passwordStr, String saltStr) {
+        char[] password = passwordStr.toCharArray();
+        byte[] salt = Base64.getDecoder().decode(saltStr);
+        PBEKeySpec spec = new PBEKeySpec(password, salt, ITERATIONS, KEY_LENGTH);
+        Arrays.fill(password, Character.MIN_VALUE);
+        try {
+            SecretKeyFactory skf = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+            byte[] hashed = skf.generateSecret(spec).getEncoded();
+            return Base64.getEncoder().encodeToString(hashed);
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+            throw new AssertionError("Error while hashing a password: " + e.getMessage(), e);
+        } finally {
+            spec.clearPassword();
+        }
+    }
+
+    public static boolean checkHash(String passwordStr, String salt, String expectedHash) {
+        char[] password = passwordStr.toCharArray();
+        String pwdHash = hash(passwordStr, salt);
+        Arrays.fill(password, Character.MIN_VALUE);
+        if (pwdHash.length() != expectedHash.length()) return false;
+        return pwdHash.equals(expectedHash);
     }
 }
