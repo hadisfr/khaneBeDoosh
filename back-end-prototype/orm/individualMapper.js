c785e271c12f0ae1786ae9f1c331ba02b7f9fefa
Mohammad Reza <mthcom@live.com>
2018-05-17 21:35:13 +0430

    add individual mapper

---
+++ back-end-prototype/orm/individualMapper.js
@@ -0,0 +1,32 @@
+var dbconfig = require('./dbconfig.js');
+var debug = require('debug')('khanebedoosh:models');
+const sqlite3 = require('sqlite3').verbose();
+
+class individualMapper{
+    getByUsername(username) {
+        let db = new sqlite3.Database(dbconfig.path, sqlite3.OPEN_READWRITE, (err) => {
+            if (err) {
+                return debug(err.message);
+            }
+            debug('Connected to the SQlite database.');
+        });
+
+        let query = 'select * from ' + dbconfig.IndividualTableName + ' where ' + dbconfig.UsernameKey + ' =?';
+
+        db.each(query, [username], (err, row) => {
+            if(err){
+                throw err;
+            }
+
+        });
+
+        db.close((err) => {
+            if (err) {
+                return debug(err.message);
+            }
+            debug('Close the database connection.');
+        });
+    }
+}
+
+module.exports = individualMapper;
