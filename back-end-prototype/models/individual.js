01e7d62c82612a3a7ad5ba2d5caf297436f5def6
h@di <hadi@hadisafari.ir>
2018-05-17 12:03:02 +0430

    add payment logic to Individual

--- back-end-prototype/models/individual.js
+++ back-end-prototype/models/individual.js
@@ -1,13 +1,15 @@
 'use strict';
 const User = require('../models/user');
-const HashMap = require('hashmap');
+
+const PHONE_PRICE = 1000;
 
 class Individual extends User {
-    constructor(username, displayName, balance) {
+    constructor(username, displayName, balance, paidHouses) {
         super(username);
         this._displayName = displayName;
         this._balance = balance;
-        this._paidHouses = new HashMap();
+        this._paidHouses = paidHouses || new Map();
+        this._paidHouses.set(566, new Set([123]));
     }
     get displayName() {
         return this._displayName;
@@ -19,13 +21,34 @@ class Individual extends User {
         return this._balance;
     }
     set balance(balance) {
-        this._balance = balance;
+        if (balance < 0) throw 'Negative Balace';
+        this._balance = balance >= 0 ? balance : 0;
     }
     get json() {
         var res = {};
         res.displayName = this.displayName;
         res.balance = this.balance;
         return { ...super.json, ...res };
+    }
+    get paidHouses() {
+        return this._paidHouses;
+    }
+    hasPaidForHouse(houseId, ownerId) {
+        return (
+            this.paidHouses.has(ownerId) &&
+            this.paidHouses.get(ownerId).has(houseId)
+        );
+    }
+    payForHouse(houseId, ownerId) {
+        try {
+            this.balance -= PHONE_PRICE;
+            if (!this.paidHouses.has(ownerId))
+                this.paidHouses.set(ownerId, new Set());
+            this.paidHouses.get(ownerId).add(houseId);
+            return true;
+        } catch (err) {
+            return false;
+        }
     }
 }
 
