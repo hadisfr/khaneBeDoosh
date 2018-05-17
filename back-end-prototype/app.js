7167cb0e782747d9faa4f9d07583d9394a05e153
h@di <hadi@hadisafari.ir>
2018-05-17 19:53:43 +0430

    enable express.urlencoded.extended

--- back-end-prototype/app.js
+++ back-end-prototype/app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var cors = require('cors')
+var cors = require('cors');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
@@ -16,7 +16,7 @@ app.set('logLevel', process.env.LOG_LEVEL || 'tiny');
 app.use(logger(app.get('logLevel')));
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
