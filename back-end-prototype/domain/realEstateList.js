a640edfa7e77c5477b27bea2ff031f10857620a2
Mohammad Reza <mthcom@live.com>
2018-05-22 21:50:27 +0430

    add realEstateList

---
+++ back-end-prototype/domain/realEstateList.js
@@ -0,0 +1,28 @@
+'use strict';
+const RealEstateAcm = require('./RealEstateAcm');
+
+class RealEstateList {
+    constructor() {
+        if (!RealEstateList.instance) {
+            this.list = {};
+            RealEstateList.instance = this;
+        }
+        return RealEstateList.instance;
+    }
+
+    addMember(member, key) {
+        this.list.key = member;
+    }
+
+    isRealEstate(name) {
+        return this.list.hasOwnProperty(name);
+    }
+
+    getRealEstate(name) {
+        return this.list[name];
+    }
+}
+
+const instance = new RealEstateList();
+
+module.exports = instance;
