c922d769700b4fc511056a19de2c3506c2d61758
h@di <hadi@hadisafari.ir>
2018-06-16 07:57:34 +0430

    add password for default users as comment

--- back-end/migrate/migrate.sql
+++ back-end/migrate/migrate.sql
@@ -84,8 +84,10 @@ CREATE TABLE "PaidHouses" (
 
 INSERT INTO "Individual" ("username","displayName","balance","isAdmin","passwordHash","passwordSalt")
     VALUES ("behnam","بهنام همایون","2000", 0, "UM6RigwvZ2GbE5G5+fplRYcMHvUG38grN6lJmPfjanQ=", "smCGaStCKSYRr1aRSRdEEboMa4ectuC5lP1y42/nCVw=");
+--    pass: maytheforcebewithyou
 INSERT INTO "Individual" ("username","displayName","balance","isAdmin","passwordHash","passwordSalt")
     VALUES ("admin","ادمین","1000", 1, "UM6RigwvZ2GbE5G5+fplRYcMHvUG38grN6lJmPfjanQ=", "smCGaStCKSYRr1aRSRdEEboMa4ectuC5lP1y42/nCVw=");
+--    pass: maytheforcebewithyou
 REPLACE INTO "House" ("houseId","ownerId","area","imageUrl","address","phone","description","buildingType","dealType","priceBase","priceRent","priceSell")
     VALUES ('sdfghjkcxjhjkojhlkmknzvnsdjlkfalkdnamxjamoddmodm','behnam','100','','UT','09123456789','?!!','VILLA','SELL',0,0,'100');
 INSERT INTO "PaidHouses" ("individualId","ownerId","houseId")
